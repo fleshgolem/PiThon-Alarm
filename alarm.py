4094eb12a27be15b287407cbd68227f225d8faad
Sören Busch <fleshgolem@gmx.net>
2012-09-24 18:54:50 +0200

    implemented mpd

--- alarm.py
+++ alarm.py
@@ -1,45 +1,62 @@
-from flask import Flask, session, redirect, url_for, escape, request, flash
+from flask import Flask, redirect, request, flash
 from flask import render_template
 from datetime import datetime
 from apscheduler.scheduler import Scheduler
 from random import randrange
+from mpd import MPDClient
+from socket import error as SocketError
 
 app = Flask(__name__)
 sched = Scheduler()
 sched.start()
-def test():
-	print "yeah"
+
+HOST = 'localhost'
+PORT = '6600'
+PASSWORD = 'dreck'
+
+mpd = MPDClient()
+
+
+def play():
+  try:
+    if PASSWORD:
+      mpd.connect(host=HOST, port=PORT, PASSWORD=PASSWORD)
+    else:
+      mpd.connect(host=HOST, port=PORT) 
+    mpd.play()
+  except SocketError:
+    print "Connection Error"
 
 def extract_date_name(job):
-	return (job.name, str(job.trigger.run_date))
+  return (job.name, str(job.trigger.run_date))
 
 def extract_dates_names(jobs):
-	return [extract_date_name(x) for x in jobs]
+  return [extract_date_name(x) for x in jobs]
 
 @app.route("/", methods=['GET','POST'])
 def main_page():
-	print ("a")
-	if request.method == 'POST':
-		print request.form['hour']
-		print request.form['minute']
-		jobdate = datetime(int(request.form['year']), int(request.form['month']), int(request.form['day']), int(request.form['hour']), int(request.form['minute']))
-		print jobdate
-		sched.add_date_job(test, jobdate, name = randrange(1, 1000000000))
-	
-		flash('Alarm added')
+  print ("a")
+  if request.method == 'POST':
+    print request.form['hour']
+    print request.form['minute']
+    jobdate = datetime(int(request.form['year']), int(request.form['month']), int(request.form['day']), int(request.form['hour']), int(request.form['minute']))
+    print jobdate
+    sched.add_date_job(play, jobdate, name = randrange(1, 1000000000))
+  
+    flash('Alarm added')
 
 
-	job_date_times = extract_dates_names(sched.get_jobs())
-	return render_template('alarm.html', job_date_times=job_date_times)
+  job_date_times = extract_dates_names(sched.get_jobs())
+  return render_template('alarm.html', job_date_times=job_date_times)
 
 @app.route("/delete/<jobname>")
 def delete(jobname):
-	filtered = [job for job in sched.get_jobs() if str(job.name)==str(jobname)]
-	for job in sched.get_jobs():
-		print job.name
-	sched.unschedule_job(filtered[0])
-	return redirect('/')
+  filtered = [job for job in sched.get_jobs() if str(job.name)==str(jobname)]
+  for job in sched.get_jobs():
+    print job.name
+  sched.unschedule_job(filtered[0])
+  return redirect('/')
 
 if __name__ == "__main__":
-	app.secret_key = 'Soeren is the gr3atest OMGWAT'
-	app.run(debug=True,host='0.0.0.0')
+  app.secret_key = 'Soeren is the gr3atest OMGWAT'
+  app.run(debug=True,host='0.0.0.0')
